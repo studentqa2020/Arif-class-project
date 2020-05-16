9a2035ab21c59d88b4f42235dde094c7fd2406da
Arif <Arif@Arifur>
2020-05-15 23:34:59 -0400

    Arif Class project 2nd version

---
+++ Automation_Selenium_Class_Project/src/main/java/com/generic/code/BaseLogin.java
@@ -0,0 +1,64 @@
+package com.generic.code;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+
+import com.config.BaseConfig;
+import com.page.object.model.LoginPage;
+import com.util.Highlighter;
+import com.util.TakeAppScreenShot;
+import com.util.Wait;
+
+public class BaseLogin {
+
+	
+	
+	public static void getLogin() throws Throwable {
+		
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
+		Logger.getLogger("org.openqa.selenium").setLevel(Level.OFF);
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		
+		driver.get(BaseConfig.getconfig("URL"));
+		
+		LoginPage login =new LoginPage(driver);
+		
+		System.out.println(driver.getTitle());
+		new Highlighter().getcolor(driver, login.getLogin());
+		TakeAppScreenShot.captureScreenShot(driver, "Cookies page");
+		
+		login.getCookies().click();
+		
+		login.getLogin().click();
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getTitle());
+
+		Wait.getExplicitWaitClickable(driver, login.getEmail());
+		new Highlighter().getcolor(driver, login.getEmail(), "green");
+		login.getEmail().sendKeys(BaseConfig.getconfig("email"));
+		TakeAppScreenShot.captureScreenShot(driver, "Email success");
+		
+		
+		new Highlighter().getcolor(driver, login.getPassword(),"blue");
+		login.getPassword().sendKeys(BaseConfig.getconfig("password"));
+		TakeAppScreenShot.captureScreenShot(driver, "Password success");
+		Thread.sleep(3000);
+
+		login.getSubmit().click();
+		TakeAppScreenShot.captureScreenShot(driver, "Login success");
+		System.out.println(driver.getTitle());
+		
+		driver.quit();
+
+		
+	}
+}
